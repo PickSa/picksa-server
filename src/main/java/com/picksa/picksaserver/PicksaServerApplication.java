d10f63d0bf53d1ffb71beb124aa05451a45d5fa9
na-yk <ninghao17@naver.com>
2023-09-02 16:59:38 +0900

    init: #1 프로젝트 초기 설정

---
+++ src/main/java/com/picksa/picksaserver/PicksaServerApplication.java
@@ -0,0 +1,13 @@
+package com.picksa.picksaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicksaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicksaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picksa/picksaserver/PicksaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picksa.picksaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicksaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
