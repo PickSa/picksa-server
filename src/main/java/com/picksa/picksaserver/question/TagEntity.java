fa070b63d063e38256e7b7d391162e7320765a6c
na-yk <ninghao17@naver.com>
2023-11-11 12:54:06 +0900

    feat: #3 question 도메인 엔티티 추가

---
+++ src/main/java/com/picksa/picksaserver/question/TagEntity.java
@@ -0,0 +1,41 @@
+package com.picksa.picksaserver.question;
+
+import com.picksa.picksaserver.global.domain.Part;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "tags")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TagEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private int generation;
+
+    @Column(nullable = false)
+    private Part part;
+
+    @Builder
+    public TagEntity(String content, int generation, Part part) {
+        this.content = content;
+        this.generation = generation;
+        this.part = part;
+    }
+
+}
