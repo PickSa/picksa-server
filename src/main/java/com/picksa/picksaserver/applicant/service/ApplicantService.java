752c473816d12d4dfb0f45aed89290dd267c402f
na-yk <ninghao17@naver.com>
2023-11-18 05:18:45 +0900

    feat: #5 지원자 전체 조회 기능 Service 추가

---
+++ src/main/java/com/picksa/picksaserver/applicant/service/ApplicantService.java
@@ -0,0 +1,35 @@
+package com.picksa.picksaserver.applicant.service;
+
+import com.picksa.picksaserver.applicant.OrderCondition;
+import com.picksa.picksaserver.applicant.dto.response.ApplicantAllResponse;
+import com.picksa.picksaserver.applicant.dto.response.ApplicantResponse;
+import com.picksa.picksaserver.applicant.repository.ApplicantQueryRepository;
+import com.picksa.picksaserver.global.domain.Generation;
+import com.picksa.picksaserver.manager.repository.ManagerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Year;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicantService {
+
+    private final ApplicantQueryRepository applicantQueryRepository;
+    private final ManagerRepository managerRepository;
+
+    private final Year thisYear = Year.now();
+
+    public ApplicantAllResponse getAllApplicants(OrderCondition orderCondition) {
+        int generation = Generation.from(thisYear).getGeneration();
+        int managerCount = managerRepository.countByGeneration(generation);
+        List<ApplicantResponse> applicants = applicantQueryRepository.findAllApplicants(orderCondition, generation);
+        return ApplicantAllResponse.of(
+                generation,
+                managerCount,
+                applicants
+        );
+    }
+
+}
