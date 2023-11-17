246b392a8f139ab29764f5a96b7be3c5f1ccd30b
na-yk <ninghao17@naver.com>
2023-11-18 05:12:02 +0900

    comment: #5 면접 가능 시간 필드 추후 처리를 위해 주석 처리

--- src/main/java/com/picksa/picksaserver/applicant/ApplicantEntity.java
+++ src/main/java/com/picksa/picksaserver/applicant/ApplicantEntity.java
@@ -1,16 +1,13 @@
 package com.picksa.picksaserver.applicant;
 
 import com.picksa.picksaserver.global.domain.Part;
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -73,12 +70,13 @@ public class ApplicantEntity {
     @Enumerated(EnumType.STRING)
     private Result result;
 
-    @ElementCollection
-    @CollectionTable(name = "interview_availables", joinColumns = @JoinColumn(name = "applicant_id"))
-    private List<InterviewAvailableTime> interviewAvailableTimes;
+//    @ElementCollection
+//    @CollectionTable(name = "interview_availables", joinColumns = @JoinColumn(name = "applicant_id"))
+//    private List<InterviewAvailableTime> interviewAvailableTimes;
+// TODO: 2023-11-18 면접 가능 시간 처리
 
     @Builder
-    public ApplicantEntity(String name, String studentId, String semester, String gender, String phone, String email, String major, String multiMajor, Part part, int generation, String portfolio, int score, int evaluator, boolean isEvaluated, Result result, List<InterviewAvailableTime> interviewAvailableTimes) {
+    public ApplicantEntity(String name, String studentId, String semester, String gender, String phone, String email, String major, String multiMajor, Part part, int generation, String portfolio, int score, boolean isEvaluated, Result result, List<InterviewAvailableTime> interviewAvailableTimes) {
         this.name = name;
         this.studentId = studentId;
         this.semester = semester;
@@ -93,7 +91,7 @@ public class ApplicantEntity {
         this.score = score;
         this.isEvaluated = isEvaluated;
         this.result = result;
-        this.interviewAvailableTimes = interviewAvailableTimes;
+//        this.interviewAvailableTimes = interviewAvailableTimes;
     }
 
 }
