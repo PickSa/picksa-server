f618ad996ff50e55f24cdb26eee3b73903478ae6
na-yk <ninghao17@naver.com>
2023-11-19 01:19:49 +0900

    feat: #7 지원자 파트별 조회 기능 Service 추가

--- src/main/java/com/picksa/picksaserver/applicant/service/ApplicantService.java
+++ src/main/java/com/picksa/picksaserver/applicant/service/ApplicantService.java
@@ -5,6 +5,7 @@ import com.picksa.picksaserver.applicant.dto.response.ApplicantAllResponse;
 import com.picksa.picksaserver.applicant.dto.response.ApplicantResponse;
 import com.picksa.picksaserver.applicant.repository.ApplicantQueryRepository;
 import com.picksa.picksaserver.global.domain.Generation;
+import com.picksa.picksaserver.global.domain.Part;
 import com.picksa.picksaserver.manager.repository.ManagerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,15 +21,34 @@ public class ApplicantService {
     private final ManagerRepository managerRepository;
 
     public ApplicantAllResponse getAllApplicants(OrderCondition orderCondition) {
-        Year thisYear = Year.now();
-        int generation = Generation.from(thisYear).getNumber();
-        int managerCount = managerRepository.countByGeneration(generation);
+        int generation = getGeneration();
+        int managerCount = getManagerCount(generation);
         List<ApplicantResponse> applicants = applicantQueryRepository.findAllApplicants(orderCondition, generation);
         return ApplicantAllResponse.of(
                 generation,
                 managerCount,
                 applicants
         );
+    }
+
+    public ApplicantAllResponse getApplicantsByPart(Part part, OrderCondition orderCondition) {
+        int generation = getGeneration();
+        int managerCount = getManagerCount(generation);
+        List<ApplicantResponse> applicants = applicantQueryRepository.findApplicantsByPart(part, orderCondition, generation);
+        return ApplicantAllResponse.of(
+                generation,
+                managerCount,
+                applicants
+        );
+    }
+
+    private int getGeneration() {
+        Year year = Year.now();
+        return Generation.from(year).getNumber();
+    }
+
+    private int getManagerCount(int generation) {
+        return managerRepository.countByGeneration(generation);
     }
 
 }
