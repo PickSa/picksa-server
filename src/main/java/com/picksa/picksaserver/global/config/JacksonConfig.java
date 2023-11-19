53e60d164b30f1df1a065dedbbf6237da9a76a05
devrokket <rokany@cau.ac.kr>
2023-11-19 13:17:25 +0900

    chore: JacksonNullable 사용을 위해 JacksonConfig 추가:

---
+++ src/main/java/com/picksa/picksaserver/global/config/JacksonConfig.java
@@ -0,0 +1,22 @@
+package com.picksa.picksaserver.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jdk8Module());
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new ParameterNamesModule());
+        return objectMapper;
+    }
+}
+
