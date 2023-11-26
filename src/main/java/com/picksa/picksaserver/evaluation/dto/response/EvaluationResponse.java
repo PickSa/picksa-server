236d6dd1e8e43f6650c06dbee9d56ec58d28eb1b
devrokket <rokany@cau.ac.kr>
2023-11-26 17:02:46 +0900

    fix#24: evaluationdto에 운영진 이름 추가

--- src/main/java/com/picksa/picksaserver/evaluation/dto/response/EvaluationResponse.java
+++ src/main/java/com/picksa/picksaserver/evaluation/dto/response/EvaluationResponse.java
@@ -5,13 +5,15 @@ import com.picksa.picksaserver.evaluation.EvaluationEntity;
 public record EvaluationResponse(
         Long evaluationId,
         Long managerId,
+        String name,
         Boolean pass,
         String comment
 ) {
     public static EvaluationResponse of(EvaluationEntity evaluation) {
         return new EvaluationResponse(
-            evaluation.getWriter().getId(),
             evaluation.getId(),
+            evaluation.getWriter().getId(),
+            evaluation.getWriter().getName(),
             evaluation.getPass(),
             evaluation.getComment()
         );
