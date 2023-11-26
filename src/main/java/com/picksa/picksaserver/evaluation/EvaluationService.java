6fdf743f53fd76c95ae86249c25a26cc4137d459
devrokket <rokany@cau.ac.kr>
2023-11-26 11:08:33 +0900

    refactor: 평가 responseDto에 정적 팩토리 메소드 적용

--- src/main/java/com/picksa/picksaserver/evaluation/EvaluationService.java
+++ src/main/java/com/picksa/picksaserver/evaluation/EvaluationService.java
@@ -37,12 +37,12 @@ public class EvaluationService {
 
         EvaluationEntity saved = evaluationRepository.save(evaluation);
 
-        return mapResponse(saved);
+        return EvaluationResponse.createEvaluationResponse(saved);
     }
 
     public EvaluationResponse getEvaluation(Long evaluationId) {
         EvaluationEntity evaluation = evaluationRepository.findByIdOrThrow(evaluationId);
-        return mapResponse(evaluation);
+        return EvaluationResponse.createEvaluationResponse(evaluation);
     }
 
     public EvaluationResponse updateEvaluation(Long evaluationId, Long managerId, EvaluationRequest request) {
@@ -58,15 +58,7 @@ public class EvaluationService {
 
         EvaluationEntity updated = evaluationRepository.save(evaluation);
 
-        return mapResponse(updated);
-    }
-
-    private EvaluationResponse mapResponse (EvaluationEntity entity) {
-        return new EvaluationResponse(
-            entity.getId(),
-            entity.getPass(),
-            entity.getComment()
-        );
+        return EvaluationResponse.createEvaluationResponse(updated);
     }
 
 }
--- src/main/java/com/picksa/picksaserver/evaluation/dto/response/EvaluationResponse.java
+++ src/main/java/com/picksa/picksaserver/evaluation/dto/response/EvaluationResponse.java
@@ -1,10 +1,18 @@
 package com.picksa.picksaserver.evaluation.dto.response;
 
+import com.picksa.picksaserver.evaluation.EvaluationEntity;
+
 public record EvaluationResponse (
     Long evaluationId,
     Boolean pass,
     String comment
-)
-{
+) {
+    public static EvaluationResponse createEvaluationResponse(EvaluationEntity evaluation) {
+        return new EvaluationResponse(
+            evaluation.getId(),
+            evaluation.getPass(),
+            evaluation.getComment()
+        );
+    }
 
 }
