4e7c04e98beaa9c08b4e15f7ee0598f90872e270
soeun <separk314@naver.com>
2023-12-12 21:07:39 +0900

    #42 chore: 코드 정리

--- src/main/java/com/picksa/picksaserver/question/repository/QuestionRepositoryImpl.java
+++ src/main/java/com/picksa/picksaserver/question/repository/QuestionRepositoryImpl.java
@@ -23,12 +23,6 @@ import static com.picksa.picksaserver.question.QQuestionEntity.questionEntity;
 @Transactional(readOnly = true)
 public class QuestionRepositoryImpl implements QuestionQueryRepository {
 
-    /*
-    사용자는 QuestionRepository 인터페이스를 DI 받아서 사용한다.
-    파일명이 Repository interface 이름 + Impl 인 클래스를 찾아서 -> interface에 JpaRepository를 주입할 때 Impl 객체를 삽입해준다.
-    => 따라서 반드시 Impl 클래스는 interface 이름 + Impl로 작성해야 한다.
-     */
-
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
@@ -84,4 +78,5 @@ public class QuestionRepositoryImpl implements QuestionQueryRepository {
                             questionEntity.sequence.asc())
                     .fetch();
     }
+
 }
