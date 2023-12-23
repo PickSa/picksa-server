dcc8ee70c79630616983fdaa7ccda9e2fac00ce9
na-yk <ninghao17@naver.com>
2023-12-23 14:54:20 +0900

    feat: #21 AnswerEntity 추가

---
+++ src/main/java/com/picksa/picksaserver/applicant/AnswerEntity.java
@@ -0,0 +1,46 @@
+package com.picksa.picksaserver.applicant;
+
+import com.picksa.picksaserver.question.QuestionEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "answers")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AnswerEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "applicant_id", nullable = false)
+    private ApplicantEntity applicant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id", nullable = false)
+    private QuestionEntity question;
+
+    @Builder
+    public AnswerEntity(String content, ApplicantEntity applicant, QuestionEntity question) {
+        this.content = content;
+        this.applicant = applicant;
+        this.question = question;
+    }
+
+}
