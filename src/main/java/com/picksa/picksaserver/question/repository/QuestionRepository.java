40fa9e5373a83f91efe020e2e052e236147b415d
na-yk <ninghao17@naver.com>
2024-02-12 04:54:47 +0900

    feat: #86 이번 기수의 질문 조회 repository 메서드 구현

--- src/main/java/com/picksa/picksaserver/question/repository/QuestionRepository.java
+++ src/main/java/com/picksa/picksaserver/question/repository/QuestionRepository.java
@@ -4,4 +4,5 @@ import com.picksa.picksaserver.question.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Long>, QuestionQueryRepository {
+
 }
