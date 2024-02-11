09f2e05f28f0c192769e28b4ed88f72974a0a7d3
na-yk <ninghao17@naver.com>
2024-02-12 02:52:49 +0900

    chore: #83 JpaAuditing config 분리

--- src/main/java/com/picksa/picksaserver/PicksaServerApplication.java
+++ src/main/java/com/picksa/picksaserver/PicksaServerApplication.java
@@ -2,10 +2,8 @@ package com.picksa.picksaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class PicksaServerApplication {
 
     public static void main(String[] args) {
